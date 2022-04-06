bfdd3ebaa18da7e96e4d02e9f5604a2deec7fb52
Matt Kolbach <mattkolbach@yahoo.com>
2022-04-05 19:36:46 -0500

    implimented webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack"); //we need this so plugins will work
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 //First, we need to create the main configuration object within our file. We'll write options 
 //within this object that tell webpack what to do. As of webpack version 4, a config file is 
@@ -8,16 +9,48 @@ const webpack = require("webpack"); //we need this so plugins will work
 //For a basic configuration, we need to provide webpack with three properties: entry, output, and mode. 
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + "/dist",
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                     loader: 'file-loader',
+                     options: {
+                         esModule: false,
+                         name (file) {
+                             return "[path][name].[ext]"
+                         },
+                         publicPath: function(url) {
+                             return url.replace("../", "/assets/")
+                         }
+                     }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outputs to a HTML file in the dist folder
+        })
     ],
     mode: 'development'//is usually set to production
 };
